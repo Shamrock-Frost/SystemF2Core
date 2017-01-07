f9121d5ff8836f4d3f25a15e0c7f5a7619c9219e
Brendan Murphy <shamrockfrost@gmail.com>
2017-01-06 17:33:45 -0800

    Implemented the Simply Typed λ Calculus+Natural Numbers See the -examples file for what code looks like

---
+++ stlc-example.rkt
@@ -0,0 +1,3 @@
+#lang s-exp "./stlc.rkt"
+((λ[add2 : (→ Nat Nat)] (add2 -5)) ;equivalent to (let [add2 ...] (add2 5))
+ (λ[x : Nat] (S (S x)))) ;adds two to its argument
---
+++ stlc.rkt
@@ -0,0 +1,25 @@
+#lang turnstile/lang
+(provide (type-out → Nat) λ #%app #%datum (typed-out [[add1 : (→ Nat Nat)] S]))
+
+(define-type-constructor → #:arity = 2)
+(define-typed-syntax (λ (x:id : τ_in:type) e) ≫
+  [[x ≫ x- : τ_in.norm] ⊢ e ≫ e- ⇒ τ_out]
+  ------
+  [⊢ (λ- (x-) e-) ⇒ (→ τ_in.norm τ_out)])
+(define-typed-syntax (#%app e_fn e_arg) ≫
+  [⊢ e_fn ≫ e_fn- ⇒ (~→ τ_in τ_out)]
+  [⊢ e_arg ≫ e_arg- ⇐ τ_in] 
+  ------
+  [⊢ (#%app- e_fn- e_arg-) ⇒ τ_out])
+(define-base-type Nat)
+(define-primop add1 : (→ Nat Nat))
+(define-typed-syntax #%datum
+  [(_ . n:nat) ≫
+   ------
+   [⊢ (#%datum- . n) ⇒ Nat]]
+  [(_ . n:integer) ≫
+   ------
+   [_ #:error (type-error #:src #'n #:msg "Unsupported literal: ~v. Only natural numbers are supported" #'n)]]
+  [(_ . x) ≫
+   --------
+   [_ #:error (type-error #:src #'x #:msg "Unsupported literal: ~v" #'x)]])
